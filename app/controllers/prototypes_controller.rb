15b791eba8e066353bcd758a14689766a8e1ea0f
Na-asai <b98911218@gmail.com>
2022-05-31 00:18:01 +0900

    微修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,6 +1,8 @@
 class PrototypesController < ApplicationController
+  before_action :base_prototype, except: [:index, :new, :create] 
   before_action :authenticate_user!, except: [:index, :show]
   before_action :move_to_root_path, except: [:index, :show]
+
 
   def index
     @prototypes = Prototype.includes(:user)
@@ -20,17 +22,14 @@ class PrototypesController < ApplicationController
   end
 
   def show
-    @prototype = Prototype.find(params[:id])
     @comment = Comment.new
     @comments = @prototype.comments
   end
 
   def edit
-    @prototype = Prototype.find(params[:id])
   end
 
   def update
-    @prototype = Prototype.find(params[:id])
     if @prototype.update(prototype_params)
       redirect_to prototype_path(@prototype)
     else
@@ -39,7 +38,6 @@ class PrototypesController < ApplicationController
   end
 
   def destroy
-    @prototype = Prototype.find(params[:id])
     if @prototype.destroy
       redirect_to root_path
     else
@@ -49,12 +47,16 @@ class PrototypesController < ApplicationController
 
   private
 
+  def base_prototype
+    @prototype = Prototype.find(params[:id])
+  end
+
   def prototype_params
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
 
   def move_to_root_path
-    unless user_signed_in?
+    unless current_user == @prototype.user
       redirect_to root_path
     end
   end
